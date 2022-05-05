b03fe3a271e549e1c7b163a99f88a4dd619cf395
Jacky <jjackyzheng@gmail.com>
2022-05-05 10:10:48 -0700

    Added basic download functionality.

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,10 @@
 import { Express, Request, Response } from 'express'
+import { downloadHandler } from './controller/download.controller'
 import { uploadHandler } from './controller/upload.controller'
 
 function routes(app: Express) {
     app.get('/health', (req: Request, res: Response) => res.sendStatus(200))
+    app.get('/download/:file', downloadHandler)
     app.post('/upload', uploadHandler)
 }
 
